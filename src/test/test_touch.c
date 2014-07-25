7cd8e4eb77db8bd4e622101953051e29203a9a43
jpgvandijk <jeroen-van-dijk@hotmail.com>
2014-07-25 10:37:26 +0200

    Added gesture recognition
    Touch test updated with recognition for tap, double-tap, hold and
    swypes.

--- src/test/test_touch.c
+++ src/test/test_touch.c
@@ -17,13 +17,20 @@
 #include "touch.h"
 #include "tft.h"
 
+// Definitions
+#define TASK_TOUCH_PERIOD		20
+#define ABS(x)					((x) < 0 ? (-(x)) : (x))
+
 // Includes (data)
 #include "data/SystemFont.h"
 
 // Structures
-typedef struct {
-	uint16_t x;
-	uint16_t y;
+typedef union {
+	struct {
+		uint16_t x;
+		uint16_t y;
+	};
+	uint32_t signal;
 } Coordinate_TypeDef;
 
 // Function prototypes
@@ -33,7 +40,7 @@ void EXTI9_5_IRQHandler (void);
 
 // Global variables
 KERNEL_TaskTableEntry * taskTouch;
-const KERNEL_TaskDescriptor TaskDescriptor_TaskTouchTFT = {TaskTouchTFT, 0, (KERNEL_MinimumTaskStackSpace + 64), 1, "TFT"};
+const KERNEL_TaskDescriptor TaskDescriptor_TaskTouchTFT = {TaskTouchTFT, 0, (KERNEL_MinimumTaskStackSpace + 128), 1, "TFT"};
 const KERNEL_TaskDescriptor TaskDescriptor_TaskTouch = {TaskTouch, 0, (KERNEL_MinimumTaskStackSpace + 64), 0, "Touch"};
 
 /***********************************************************************************/
@@ -115,12 +122,22 @@ void touch_test (void)
 	KERNEL_Start();
 }
 
+void debug (char * text)
+{
+	TFT_Color = 0xFFFF;
+	TFT_SetArea(0, 319, 0, 239);
+	TFT_Color = 0x0000;
+	TFT_SelectFont(SystemFont);
+	TFT_PrintString(5, 5, text);
+}
+
 void TaskTouchTFT (uint32_t arg)
 {
-	// Local buffer
-	Coordinate_TypeDef coordinate;
-	Coordinate_TypeDef coordinate_old;
+	// Local variables
+	uint32_t action = 0;
 	uint32_t newdata;
+	uint32_t time;
+	Coordinate_TypeDef coordinate, start, coordinate_old;
 
 	// Init the TFT (in task, since svc delays required)
 	TFT_Init();
@@ -129,35 +146,139 @@ void TaskTouchTFT (uint32_t arg)
 	TFT_Color = 0xFFFF;
 	TFT_SetArea(0, 319, 0, 239);
 
-	// Set a pixel for each touch measurement
+	// Analyse the touch measurements
 	while (1)
 	{
-		QUEUE_Lock();
-		if (QUEUE_IsNotEmpty())
-		{
-			coordinate = QUEUE_Receive();
-			newdata = 1;
-		}
-		QUEUE_Unlock();
+		// Get new data
+		newdata = 0;
+		do {
+			QUEUE_Lock();
+			if (QUEUE_IsNotEmpty()) {
+				coordinate = QUEUE_Receive();
+				newdata = 1;
+			}
+			QUEUE_Unlock();
+		} while (!newdata);
 
-		if (newdata)
+		if ((newdata) && (coordinate.signal != 0xFFFFFFFF)) {
+			if (coordinate_old.signal == 0xFFFFFFFF)
+				coordinate_old = coordinate;
+			
+			// Draw line
+			TFT_Color = 0x0000;
+			TFT_SetLine(coordinate_old.x, coordinate_old.y, coordinate.x, coordinate.y);
+		}
+		coordinate_old = coordinate;
+		
+		// End of touch?
+		if (coordinate.signal == 0xFFFFFFFF)
 		{
-			if ((coordinate.x == 0xFFFF) && (coordinate.y == 0xFFFF))
-			{
-				TFT_Color = 0xFFFF;
-				TFT_SetArea(0, 319, 0, 239);
-				newdata = 0;
+			uint32_t newaction = 0;
+			
+			// Report result
+			if (action == 100) {
+				debug("");
+			} else if (action == 1) {
+				if (time >= 1) {
+					//debug("Tap or double-tap");
+					KERNEL_SVCForceContextSwitchDelay(250);
+					newdata = 0;
+					QUEUE_Lock();
+					if (QUEUE_IsNotEmpty()) {
+						coordinate = QUEUE_Receive();
+						newdata = 1;
+					}
+					QUEUE_Unlock();
+					
+					int16_t dx = coordinate.x - start.x;
+					int16_t dy = coordinate.y - start.y;
+					if ((newdata) && (ABS(dx) < 25) && (ABS(dy) < 25)) {
+						debug("Double-tap");
+						newaction = 101;
+					}
+					else
+						debug("Tap");
+				}
+			} else if (action == 2) {
+				debug("End of hold");
+			} else if (action == 3) {
+				debug("Swype right");
+			} else if (action == 4) {
+				debug("Swype left");
+			} else if (action == 5) {
+				debug("Swype down");
+			} else if (action == 6) {
+				debug("Swype up");
 			}
-			else
+			
+			// Start new analysis unless double-tap
+			action = newaction;
+		}
+
+		// First touch?
+		else if (action == 0)
+		{
+			// Set variables
+			start = coordinate;
+			time = 0;
+			action = 1;
+		}
+
+		// Continued touch
+		else
+		{
+			// Determine displacement and increase time
+			int16_t dx = coordinate.x - start.x;
+			int16_t dy = coordinate.y - start.y;
+			time++;
+
+			if ((action == 1) || (action == 2))
 			{
-				if ((coordinate_old.x == 0xFFFF) && (coordinate_old.y == 0xFFFF))
-					coordinate_old = coordinate;
-				
-				TFT_Color = 0x0000;
-				TFT_SetLine(coordinate_old.x, coordinate_old.y, coordinate.x, coordinate.y);
-				newdata = 0;
+				//  Check if still at this position
+				if ((ABS(dx) < 25) && (ABS(dy) < 25)) {
+					start.x = (start.x + coordinate.x) >> 1;
+					start.y = (start.y + coordinate.y) >> 1;
+					if ((action == 1) && (time > 50)) {
+						debug("Start of hold");
+						action = 2;
+					}
+				} else {
+					if (action == 2) {
+						debug("End of hold");
+						action = 100;
+					} else {
+						if (ABS(dx) > ABS(dy)) {
+							if (dx > 0) {
+								action = 3;
+							} else {
+								action = 4;
+							}
+						} else {
+							if (dy > 0) {
+								action = 5;
+							} else {
+								action = 6;
+							}
+						}
+					}
+				}
+			} else if (action == 3) {
+				if (!(dx > 2*ABS(dy))) {
+					action = 100;
+				}
+			} else if (action == 4) {
+				if (!(-dx > 2*ABS(dy))) {
+					action = 100;
+				}
+			} else if (action == 5) {
+				if (!(dy > 2*ABS(dx))) {
+					action = 100;
+				}
+			} else if (action == 6) {
+				if (!(-dy > 2*ABS(dx))) {
+					action = 100;
+				}
 			}
-			coordinate_old = coordinate;
 		}
 	}
 }
@@ -167,35 +288,35 @@ void TaskTouch (uint32_t arg)
 	// Local buffer
 	Coordinate_TypeDef coordinate;
 	uint16_t measurement[4];
+	//uint64_t time;
 
-	// Run every 10 ms
+	// Run every TASK_TOUCH_PERIOD ms
 	while (1)
 	{
-		// Check if screen is touched
-		while (*((volatile uint32_t *) GPIOB_IDR) & (1 << 6))
+		// Check if screen is no longer touched
+		if (*((volatile uint32_t *) GPIOB_IDR) & (1 << 6))
 		{
-			// Add special coordinate to the queue
-			coordinate.x = 0xFFFF;
-			coordinate.y = 0xFFFF;
+			// Signal end of touching and start measuring the untouched time
+			coordinate.signal = 0xFFFFFFFF;
 			QUEUE_Lock();
 			if (QUEUE_IsNotFull())
 				QUEUE_Send(coordinate);
 			QUEUE_Unlock();
+			//time = KERNEL_Time;
 
-			// Store reference to the task for auto wake-up
+			// Auto wake-up when touch interrupt occurs (no time-out)
 			taskTouch = KERNEL_TaskRunning;
-			
-			// Screen is not touched, block until touch interrupt occurs
 			KERNEL_SVCIRQEnable(TOUCH_IRQ_NUMBER);
-
-			// Block with 10s timeout
-			KERNEL_SVCForceContextSwitchDelay(10000);
-
-			// Task restarted by touch or time-out
+			KERNEL_SVCForceContextSwitchIndefinitely();
 			KERNEL_SVCIRQDisable(TOUCH_IRQ_NUMBER);
-			
-			// Remove reference
 			taskTouch = 0;
+			
+			// Signal the untouched time (normalized)
+			//coordinate.signal = ((uint32_t)(KERNEL_Time - time) / TASK_TOUCH_PERIOD);
+			//QUEUE_Lock();
+			//if (QUEUE_IsNotFull())
+			//	QUEUE_Send(coordinate);
+			//QUEUE_Unlock();
 		}
 
 		// Screen is touched, measure x, y, z1 and z2
@@ -212,32 +333,35 @@ void TaskTouch (uint32_t arg)
 		}
 
 		// Only update coordinate if pressure is high enough
-		if ((pressure != 0) && (pressure < 28))
+		if ((pressure != 0) && (pressure < 30))
 		{
-			// Determine x coordinate
-			coordinate.x = 320 - (measurement[1] - 250) / 11;
-			if (coordinate.x < 0)
-				coordinate.x = 0;
-			if (coordinate.x > 319)
-				coordinate.x = 319;
+			// Only update if coordinates within bounds
+			if ((measurement[1] >= 250) && (measurement[0] >= 250) && (measurement[1] < 3770) && (measurement[0] < 3610))
+			{		
+				// Determine x coordinate
+				coordinate.x = 319 - (measurement[1] - 250) / 11;
+				if (coordinate.x < 0)
+					coordinate.x = 0;
+				if (coordinate.x > 319)
+					coordinate.x = 319;
 
-			// Determine y coordinate
-			coordinate.y = (measurement[0] - 250) / 14;
-			if (coordinate.y < 0)
-				coordinate.y = 0;
-			if (coordinate.y > 239)
-				coordinate.y = 239;
+				// Determine y coordinate
+				coordinate.y = (measurement[0] - 250) / 14;
+				if (coordinate.y < 0)
+					coordinate.y = 0;
+				if (coordinate.y > 239)
+					coordinate.y = 239;
 
-			// Add coordinate to the queue
-			QUEUE_Lock();
-			if (QUEUE_IsNotFull())
-				QUEUE_Send(coordinate);
-			QUEUE_Unlock();
+				// Add coordinate to the queue
+				QUEUE_Lock();
+				if (QUEUE_IsNotFull())
+					QUEUE_Send(coordinate);
+				QUEUE_Unlock();
+			}
 		}
 
-		// Every 10 ms
-		// FIXME: can't use periodic, since time fucked up?
-		KERNEL_SVCForceContextSwitchDelay(10);
+		// Run every TASK_TOUCH_PERIOD ms
+		KERNEL_SVCForceContextSwitchPeriodic(TASK_TOUCH_PERIOD);
 	}
 }
 
@@ -245,111 +369,10 @@ void TOUCH_IRQ_HANDLER (void)
 {
 	// Wake-up the touch task
 	if (taskTouch != 0)
-		KERNEL_AddTaskToReadyList(KERNEL_RemoveTaskFromDelayedList(taskTouch));
+		KERNEL_AddTaskToReadyList(taskTouch);
 	
 	// Clear flag
 	*((volatile uint32_t *) EXTI_PR) = (1 << 6);
 }
-
-
-
-
-/*
-while (1)
-{
-	// Measure touch
-	int32_t x, y, pressure;
-	int32_t x1, x2, y1, y2;
-	char text[10];
-
-
-	// Clear the TFT
-	TFT_Color = 0xFFFF;
-	TFT_SetArea(0, 319, 0, 239);
-	TFT_Color = 0x0000;
-
-	// Print measurement
-	TFT_SelectFont(SystemFont);
-	text[0] = 'x';
-	text[1] = ':';
-	text[2] = ' ';
-	text[3] = ' ';
-	text[4] = '0' + buffer[0] / 1000;
-	text[5] = '0' + (buffer[0] % 1000) / 100;
-	text[6] = '0' + (buffer[0] % 100) / 10;
-	text[7] = '0' + (buffer[0] % 10);
-	text[8] = 0;
-	TFT_PrintString(100, 100, text);
-	text[0] = 'y';
-	text[1] = ':';
-	text[2] = ' ';
-	text[3] = ' ';
-	text[4] = '0' + buffer[1] / 1000;
-	text[5] = '0' + (buffer[1] % 1000) / 100;
-	text[6] = '0' + (buffer[1] % 100) / 10;
-	text[7] = '0' + (buffer[1] % 10);
-	text[8] = 0;
-	TFT_PrintString(100, 116, text);
-	text[0] = 'z';
-	text[1] = '1';
-	text[2] = ':';
-	text[3] = ' ';
-	text[4] = '0' + buffer[2] / 1000;
-	text[5] = '0' + (buffer[2] % 1000) / 100;
-	text[6] = '0' + (buffer[2] % 100) / 10;
-	text[7] = '0' + (buffer[2] % 10);
-	text[8] = 0;
-	TFT_PrintString(100, 132, text);
-	text[0] = 'z';
-	text[1] = '2';
-	text[2] = ':';
-	text[3] = ' ';
-	text[4] = '0' + buffer[3] / 1000;
-	text[5] = '0' + (buffer[3] % 1000) / 100;
-	text[6] = '0' + (buffer[3] % 100) / 10;
-	text[7] = '0' + (buffer[3] % 10);
-	text[8] = 0;
-	TFT_PrintString(100, 148, text);
-
-	// Draw rectangle
-	x = 320 - (buffer[1] - 250) / 11;
-	if (x < 0) x = 0;
-	if (x > 319) x = 319;
-	y = (buffer[0] - 250) / 14;
-	if (y < 0) y = 0;
-	if (y > 239) y = 239;
-
-	pressure = 0;
-	if (buffer[2])
-	{
-		pressure = buffer[0] * 16;
-		pressure *= buffer[3] - buffer[2];
-		pressure /= buffer[2];
-		pressure /= 4096;
-	}
-
-	if ((pressure > 0) && (pressure < 30))
-	{
-		x1 = x - pressure;
-		if (x1 < 0)
-			x1 = 0;
-		x2 = x + pressure;
-		if (x2 > 319)
-			x2 = 319;
-		y1 = y - pressure;
-		if (y1 < 0)
-			y1 = 0;
-		y2 = y + pressure;
-		if (y2 > 239)
-			y2 = 239;
-		
-		TFT_Color = 0x0010;
-		TFT_SetArea(x1, x2, y1, y2);
-	}
-
-	// Wait
-	KERNEL_SVCForceContextSwitchDelay(50);
-}
-*/
 
 #endif//STM
